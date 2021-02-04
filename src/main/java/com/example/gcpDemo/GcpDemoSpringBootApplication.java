85c911ca1f9e6dc62b813f8653fa86a50fa85332
SAHIL BHATIA <sahilorn@gmail.com>
2021-02-05 01:53:16 +0530

    firstCommit

---
+++ src/main/java/com/example/gcpDemo/GcpDemoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.gcpDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpDemoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpDemoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gcpDemo/GcpDemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gcpDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpDemoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
